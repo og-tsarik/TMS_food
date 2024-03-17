ac3c3130a064804554763913c32e4712e66f125d
og-tsarik <149010719+og-tsarik@users.noreply.github.com>
2024-03-17 14:01:01 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from .views import create_ingredient, create_recipe, show_recipe, update_recipe
+from . import views
 
 # /recipe/
 
 urlpatterns = [
-    path('createingredient/', create_ingredient, name='create_ingredient'),
-    path('create/', create_recipe, name='create-recipe'),
-    path('update/<int:recipe_id>', update_recipe, name='update-recipe'),
-    path('<int:recipe_id>', show_recipe, name='show-recipe'),
+    path('createingredient/', views.create_ingredient, name='create_ingredient'),
+    path('create/', views.create_recipe, name='create-recipe'),
+    path('update/<int:recipe_id>', views.update_recipe, name='update-recipe'),
+    path('<int:recipe_id>', views.show_recipe, name='show-recipe'),
+    path("favorites", views.ListFavoriteRecipesView.as_view(), name='show-favorite-recipes'),
+    path("favorite/<int:recipe_id>", views.MakeFavoriteView.as_view(), name='make-favorite-recipe')
 ]
