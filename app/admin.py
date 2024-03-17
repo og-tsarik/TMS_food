2a68135aabc3c279e2add03f574f06046b9b9fb7
og-tsarik <149010719+og-tsarik@users.noreply.github.com>
2024-03-17 14:00:37 +0300

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.sessions.models import Session
 
-# Register your models here.
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+
+    list_display = ['session_key', '_session_data', 'expire_date']
