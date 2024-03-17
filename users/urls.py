f1a6ef0e3cb2de54d1070f1b79399f4e6a5e73a1
og-tsarik <149010719+og-tsarik@users.noreply.github.com>
2024-03-17 14:05:27 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import register_view
+from .views import register_view, confirm_register_view
 
 # /account/
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("register/", register_view, name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/confirm/<uidb64>/<token>", confirm_register_view, name="register-confirm"),
 ]
